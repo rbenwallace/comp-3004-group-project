06687975a671f73e8f67a0b4d14a1d323db2d69e
gadzyCode <45364086+gadzyCode@users.noreply.github.com>
2020-01-30 17:22:11 -0500

    Added Login, SIgn Up, Forget Password

---
+++ Android/ourHouse/app/src/main/java/com/uniques/ourhouse/LS_Main.java
@@ -0,0 +1,44 @@
+package com.uniques.ourhouse;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
+
+import android.nfc.Tag;
+import android.os.Bundle;
+import android.util.Log;
+
+public class LS_Main extends AppCompatActivity {
+
+    private static final String TAG = "LS_Main";
+
+    private SectionPageAdapter mSectionPageAdapter;
+    private CustomViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ls__main);
+
+        mSectionPageAdapter = new SectionPageAdapter(getSupportFragmentManager());
+
+        mViewPager = (CustomViewPager) findViewById(R.id.container);
+        setupViewPager(mViewPager);
+
+    }
+
+    private void setupViewPager(CustomViewPager viewPager){
+        SectionPageAdapter adapter = new SectionPageAdapter(getSupportFragmentManager());
+        //Inflates first Fragment
+        adapter.addFragment(new Login(), "Login");
+        adapter.addFragment(new Sign_Up(), "Sign Up");
+        adapter.addFragment(new Forget_Password(), "Forget Password");
+        viewPager.setAdapter(adapter);
+        Log.d("titsMagee", "CHECKING IF LOADED");
+    }
+    //Changes to the fragment with fragment number
+    public void setViewPager(int fragmentNumber){
+        mViewPager.setCurrentItem(fragmentNumber);
+    }
+
+}
