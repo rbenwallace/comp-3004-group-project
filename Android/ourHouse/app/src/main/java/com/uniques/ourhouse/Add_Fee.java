3c23e0d16df008622bd642a28297a2445a795b00
rbenwallace <45673743+rbenwallace@users.noreply.github.com>
2020-02-01 14:34:54 -0500

    Added Manage tab Fragment classes

---
+++ Android/ourHouse/app/src/main/java/com/uniques/ourhouse/Add_Fee.java
@@ -0,0 +1,39 @@
+package com.uniques.ourhouse;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class Add_Fee extends Fragment {
+    private static final String TAG = "Sign_Up";
+    private Button manageBackButton;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_add_fee, container, false);
+        Log.d(TAG, "Add Fee Clicked");
+
+        manageBackButton = (Button) view.findViewById(R.id.manage_back);
+
+        manageBackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "Back", Toast.LENGTH_LONG).show();
+                //TODO NAVIGATE TO NEXT FRAGMENT
+                ((LS_Main) getActivity()).setViewPager(3);
+            }
+        });
+
+        return view;
+    }
+}
+
